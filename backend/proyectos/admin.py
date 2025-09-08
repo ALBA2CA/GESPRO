68f61ef83fa29ad5ab9caba3b3be6061833dccd0
IsaiasCabreraG <isaiasgoku15@gmail.com>
2025-09-08 11:17:15 -0300

    añadi el admin

--- backend/proyectos/admin.py
+++ backend/proyectos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Proyecto, Actividad, LineaTrabajo, ProductoAsociado, Encargado
 
 # Register your models here.
+admin.site.register(Proyecto)
+admin.site.register(Actividad)
+admin.site.register(LineaTrabajo)
+admin.site.register(ProductoAsociado)
+admin.site.register(Encargado)
