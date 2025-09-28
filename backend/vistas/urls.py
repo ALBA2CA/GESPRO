e6a2f930ce81e40a2152aa5a938dd06b905b61bd
edu-bn <eduardo.barreran@gmail.com>
2025-09-28 15:31:39 -0300

    Cambio de estado en lista de actividades

--- backend/vistas/urls.py
+++ backend/vistas/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('vista_gantt/<int:proyecto_id>/', views.vista_gantt, name='vista_gantt'),
     path('lista_actividades/<int:proyecto_id>/', views.lista_actividades, name="lista_actividades"),
+    path("actualizar_estado/<int:actividad_id>/", views.actualizar_estado_actividad, name="actualizar_estado_actividad"),
 ]
